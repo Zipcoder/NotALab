bebf43461afc336c3874ed1d8266f0be53dd7170
miscbits <willow6786@gmail.com>
2019-02-01 13:31:26 -0500

    tests are failing

---
+++ src/main/java/Person.java
@@ -0,0 +1,34 @@
+public class Person {
+
+    private int age;
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public Person(int age) {
+        this.age = age;
+    }
+
+    public Person(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getAge() {
+        return 0;
+    }
+
+    public void setAge(int age) {
+
+    }
+
+    public String getName() {
+        return "";
+    }
+
+    public void setName(String name) {
+
+    }
+}
---
+++ src/test/java/PersonTest.java
@@ -0,0 +1,39 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PersonTest {
+
+    private Person person;
+
+    @Before
+    public void setUp() throws Exception {
+        this.person = new Person( 40, "Henry");
+    }
+
+    @Test
+    public void getAge() {
+        assertEquals(40, this.person.getAge());
+    }
+
+    @Test
+    public void setAge() {
+        this.person.setAge(30);
+
+        assertEquals(30, this.person.getAge());
+
+    }
+
+    @Test
+    public void getName() {
+        assertEquals("Henry", this.person.getName());
+    }
+
+    @Test
+    public void setName() {
+        this.person.setName("Harry");
+
+        assertEquals("Harry", this.person.getName());
+    }
+}
